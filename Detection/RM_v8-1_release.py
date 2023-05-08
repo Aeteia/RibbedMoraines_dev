86b71d2806c30e5c1d0b933fa86eb0d8cb12f72b
Aeteia <74658510+Aeteia@users.noreply.github.com>
2023-05-08 22:24:58 +0200

    Update RM_v8-1_release.py

--- Detection/RM_v8-1_release.py
+++ Detection/RM_v8-1_release.py
@@ -43,7 +43,7 @@ for filename in os.listdir(Extent_dir):
         ExtentRaster = Extent_dir + "33-" + index_fn + "_10m.tif"
         
         # Fill Holes in Raster
-        filledDEM = gdal.FillNodata(srcfile = ExtentRaster, targetBand = 1, maskBand = None
+        filledDEM = gdal.FillNodata(srcfile = ExtentRaster, targetBand = 1, maskBand = None,
                                         maxSearchDist = 30, smoothingIterations = 0)
         print(filledDEM)
         ExtentRaster = filledDEM
